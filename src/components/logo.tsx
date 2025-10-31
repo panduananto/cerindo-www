db7896934b4e2f5951a7c9d0ebb993d60a761004
unknown <panduanantoh@gmail.com>
2025-10-31 23:38:38 +0700

    feat(ui): refactor Logo component to use Next.js Image and support dynamic sources

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
+import Image from 'next/image'
 import Link from 'next/link'
 
 import { cn } from '@/lib/utils'
+
+import logo from '../../public/images/cerindo_bintang_logo.svg'
 
 type LogoProps = {
 	source?: string
 	link?: string
 }
 
-function Logo({
-	source = '/images/cerindo_bintang_logo.svg',
-	link = '/',
-	className,
-	...props
-}: LogoProps & React.HTMLAttributes<HTMLAnchorElement>) {
+function Logo({ source, link = '/', className, ...props }: LogoProps & React.HTMLAttributes<HTMLAnchorElement>) {
+	const imageSrc = source || logo
+
 	return (
 		<Link href={link} className={cn(className)} {...props}>
-			<img src={source} className="inline-block h-6 w-auto" alt="Logo" />
+			<Image src={imageSrc} className="inline-block h-6 w-auto" alt="Logo" />
 			<span className="sr-only">Home</span>
 		</Link>
 	)
